e62ee8bfce9e5e8b21bb3f98654f84d13f5bb381
Bryan Tarpley <bptarpley@gmail.com>
2025-10-16 13:35:28 -0500

    updating dependencies

--- app/wbhb/urls.py
+++ app/wbhb/urls.py
@@ -13,8 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
-from django.urls import path
+from django.conf.urls import include
+from django.urls import path, re_path
 from django.contrib import admin
 from wbhb.viewer import views as viewer_views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -23,20 +23,20 @@ admin.site.site_header = 'Beowulf Bibliography'
 admin.site.site_title = 'Beowulf Bibliography'
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^tinymce/', include('tinymce.urls')),
-    url(r'^$', viewer_views.index, name='home'),
-    url(r'^detail$', viewer_views.source_detail, name='detail'),
-    url(r'^export$', viewer_views.export, name='export'),
-    url(r'^sources$', viewer_views.sources, name='sources'),
-    url(r'^people$', viewer_views.people, name='people'),
-    url(r'^roles$', viewer_views.roles, name='roles'),
-    url(r'^locations$', viewer_views.locations, name='locations'),
-    url(r'^languages$', viewer_views.languages, name='languages'),
-    url(r'^publishers$', viewer_views.publishers, name='publishers'),
-    url(r'^fields$', viewer_views.fields, name='fields'),
-    url(r'^periods$', viewer_views.periods, name='periods'),
-    url(r'^graph$', viewer_views.relationship_graph, name='graph'),
+    re_path(r'^admin/', admin.site.urls),
+    re_path(r'^tinymce/', include('tinymce.urls')),
+    re_path(r'^$', viewer_views.index, name='home'),
+    re_path(r'^detail$', viewer_views.source_detail, name='detail'),
+    re_path(r'^export$', viewer_views.export, name='export'),
+    re_path(r'^sources$', viewer_views.sources, name='sources'),
+    re_path(r'^people$', viewer_views.people, name='people'),
+    re_path(r'^roles$', viewer_views.roles, name='roles'),
+    re_path(r'^locations$', viewer_views.locations, name='locations'),
+    re_path(r'^languages$', viewer_views.languages, name='languages'),
+    re_path(r'^publishers$', viewer_views.publishers, name='publishers'),
+    re_path(r'^fields$', viewer_views.fields, name='fields'),
+    re_path(r'^periods$', viewer_views.periods, name='periods'),
+    re_path(r'^graph$', viewer_views.relationship_graph, name='graph'),
     path('page/<str:slug>', viewer_views.page),
 ]
 
